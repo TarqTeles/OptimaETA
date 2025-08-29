a29368d7efed1227acc4e6ad3d8a1a7d056f7bfc
Tarquinio Roberto de Melo Teles <tarqteles@simplydone.com.br>
2025-08-29 12:16:33 -0300

    Initial Commit

---
+++ OptimaETA/OptimaETAApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimaETAApp.swift
+//  OptimaETA
+//
+//  Created by Tarquinio Teles on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct OptimaETAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OptimaETATests/OptimaETATests.swift
@@ -0,0 +1,17 @@
+//
+//  OptimaETATests.swift
+//  OptimaETATests
+//
+//  Created by Tarquinio Teles on 29/08/25.
+//
+
+import Testing
+@testable import OptimaETA
+
+struct OptimaETATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
